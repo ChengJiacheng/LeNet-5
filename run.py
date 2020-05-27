9f5451f92874f2ad0405b6b85748ad051baa45d4
ChengJiacheng <31717437+ChengJiacheng@users.noreply.github.com>
2020-05-27 13:04:40 -0700

    updates

--- run.py
+++ run.py
@@ -1,14 +1,17 @@
-from lenet import LeNet5
+# from lenet import LeNet5
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torchvision.datasets.mnist import MNIST
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
-import visdom
+# import visdom
 import onnx
 
-viz = visdom.Visdom()
+# viz = visdom.Visdom()
+from PIL import Image
+import numpy as np
+from network import LeNet, MLP
 
 data_train = MNIST('./data/mnist',
                    download=True,
@@ -24,7 +27,9 @@ data_test = MNIST('./data/mnist',
 data_train_loader = DataLoader(data_train, batch_size=256, shuffle=True, num_workers=8)
 data_test_loader = DataLoader(data_test, batch_size=1024, num_workers=8)
 
-net = LeNet5()
+# net = LeNet()
+net = MLP()
+
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(net.parameters(), lr=2e-3)
 
@@ -56,11 +61,11 @@ def train(epoch):
             print('Train - Epoch %d, Batch: %d, Loss: %f' % (epoch, i, loss.detach().cpu().item()))
 
         # Update Visualization
-        if viz.check_connection():
-            cur_batch_win = viz.line(torch.Tensor(loss_list), torch.Tensor(batch_list),
-                                     win=cur_batch_win, name='current_batch_loss',
-                                     update=(None if cur_batch_win is None else 'replace'),
-                                     opts=cur_batch_win_opts)
+        # if viz.check_connection():
+        #     cur_batch_win = viz.line(torch.Tensor(loss_list), torch.Tensor(batch_list),
+        #                              win=cur_batch_win, name='current_batch_loss',
+        #                              update=(None if cur_batch_win is None else 'replace'),
+        #                              opts=cur_batch_win_opts)
 
         loss.backward()
         optimizer.step()
@@ -90,6 +95,47 @@ def train_and_test(epoch):
     onnx_model = onnx.load("lenet.onnx")
     onnx.checker.check_model(onnx_model)
 
+def get_feature(embedding, loader, dim_embedding, l2_normalize=0, tencrop=False, eval=True):
+    embedding = embedding.cuda()
+    if eval:
+        embedding.eval()  # Set model to evaluate mode
+    else:
+        embedding.train()
+    features = np.zeros((len(loader.dataset), dim_embedding))
+    labels = np.zeros((0))
+
+    with torch.no_grad():
+        idx = 0
+        # Iterate over data.
+        for step, (inputs, targets) in enumerate(loader):
+            if step%100 == 0:
+                print(step)
+
+            inputs = inputs.cuda()
+            targets = targets.cuda()
+
+            if tencrop:
+                bs, ncrops, c, h, w = inputs.size()
+                # outputs = embedding(inputs.view(-1, c, h, w))  # fuse batch size and ncrops
+                outputs =  nn.parallel.data_parallel(embedding, inputs.view(-1, c, h, w))
+                outputs = outputs.view(bs, ncrops, -1).mean(1)
+            else:
+                # outputs = embedding(inputs)
+                outputs = nn.parallel.data_parallel(embedding, inputs)
+
+            if l2_normalize:
+                outputs = torch.nn.functional.normalize(outputs, p=2, dim=1)
+
+            outputs = outputs.detach().cpu().numpy().squeeze()
+            # features = np.vstack([features, outputs])
+            features[idx:idx+len(targets), :] = outputs
+            idx = idx + len(targets)
+            targets = targets.detach().cpu().numpy().squeeze()
+            labels = np.append(labels, targets)
+
+    return features, labels
+
+
 
 def main():
     for e in range(1, 16):
@@ -98,3 +144,31 @@ def main():
 
 if __name__ == '__main__':
     main()
+    # torch.save(net.state_dict(), 'net.pth')
+    # torch.save(net.state_dict(), 'mlp.pth')
+    
+    
+    # net.load_state_dict(torch.load('net.pth'))
+    # net.load_state_dict(torch.load('mlp.pth'))
+    
+    # transform=transforms.Compose([
+    #                   transforms.Resize((32, 32)),
+    #                   transforms.ToTensor()])
+    
+    # # X_train = data_train.data.numpy()
+    # # X_train = Image.fromarray(X_train)
+    
+    # import copy
+    # embedding = copy.deepcopy(net)
+    # embedding.f5 = nn.Sequential()
+
+    import copy
+    embedding = copy.deepcopy(net)
+    embedding.fc3 = nn.Sequential()
+    
+    features, labels = get_feature(embedding, data_train_loader, dim_embedding=20)
+
+    import scipy.io as sio
+    # sio.savemat("result.mat", {'X_train': features, 'y_train': labels})
+    
+    sio.savemat("result_mlp.mat", {'X_train': features, 'y_train': labels})
---
+++ test.m
@@ -0,0 +1,3 @@
+ load('result.mat')
+mardiatest(X_train, 0.05)
+
